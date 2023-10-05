a22b23cde6320cd989ac52bff10861fabb86f45a
emreakay <emreakay@users.noreply.github.com>
2023-10-05 11:35:06 +0000

    Fix styling

--- src/Contacts/StateableModelContract.php
+++ src/Contacts/StateableModelContract.php
@@ -12,24 +12,22 @@ interface StateableModelContract
 {
     /**
      * workflow attribute for model class
-     * @return string
      */
     public static function workflowAttribute(): string;
 
     /**
      * state attribute for model class (or type)
-     * @return string
      */
     public static function stateAttribute(): string;
 
     /**
      * metadata attribute for model class (or type)
-     * @return string
      */
     public static function stateMetadataAttribute(): string;
 
     /**
      * returns supported workflows of the model class (or type)
+     *
      * @return array<string>
      */
     public static function supportedWorkflows(): array;
@@ -44,52 +42,44 @@ interface StateableModelContract
 
     /**
      * Current model instance's applied workflow
-     * @return string
      */
     public function appliedWorkflow(): string;
 
     /**
      * Current model instance's current workflow
-     * @return string
      */
     public function currentState(): string;
 
     /**
      * Current metadata of the instance
+     *
      * @return array<string, mixed>
      */
     public function currentStateMetadata(): array;
 
     /**
      * check and return if
-     * @param string $state
-     * @param array<class-string>|null $withoutGuards
-     * @return bool
+     *
+     * @param  array<class-string>|null  $withoutGuards
      */
     public function canTransitionTo(string $state, array $withoutGuards = null): bool;
 
     /**
-     * @param array<class-string>|null $withoutGuards
-     * @return array|null
+     * @param  array<class-string>|null  $withoutGuards
      */
     public function possibleTransitions(array $withoutGuards = null): ?array;
 
     /**
-     * @param string $state
-     * @param string|null $comment
-     * @param ?class-string $byModelType
-     * @param ?int $byModelId
-     * @param array<string, mixed> $metadata
-     * @param array<class-string> $withoutGuards
-     * @param bool $transitionHistoryAction
-     * @return bool
+     * @param  ?class-string  $byModelType
+     * @param  ?int  $byModelId
+     * @param  array<string, mixed>  $metadata
+     * @param  array<class-string>  $withoutGuards
      */
     public function transitionTo(
-        string  $state, ?string $comment = null,
-        ?string $byModelType = null, ?int $byModelId = null,
-        ?array  $metadata = null,
-        ?array  $withoutGuards = null,
-        bool    $transitionHistoryAction = true
+        string $state, string $comment = null,
+        string $byModelType = null, int $byModelId = null,
+        array $metadata = null,
+        array $withoutGuards = null,
+        bool $transitionHistoryAction = true
     ): bool;
-
 }
