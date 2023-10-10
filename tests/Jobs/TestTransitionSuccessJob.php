0e3c459bb980ec469dc6794ed41dbf23679938f9
emreakay <emreakay@users.noreply.github.com>
2023-10-10 17:04:34 +0000

    Fix styling

--- tests/Jobs/TestTransitionSuccessJob.php
+++ tests/Jobs/TestTransitionSuccessJob.php
@@ -10,12 +10,8 @@ class TestTransitionSuccessJob extends AbstractTransitionSuccessJob
 {
     /**
      * Execute the job.
-     * @param StateableModelContract&Model $model
-     * @param string $from
-     * @param string $to
-     * @param array $parameters
      */
-    public function handle(StateableModelContract & Model $model, string $from, string $to, array $parameters = []): void
+    public function handle(StateableModelContract&Model $model, string $from, string $to, array $parameters = []): void
     {
     }
 }
